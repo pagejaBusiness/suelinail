ad36f4eb50ea79743e819d82d813ead780511aa8
WilliamAraujo <w67543210@gmail.com>
2025-09-19 22:01:08 -0300

    fix: imagens otimizadas

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import Img1 from "../Images/Slider.webp";
-import Img2 from "../Images/Slider2.webp";
-import Img3 from "../Images/Slider3.webp";
-import Img4 from "../Images/Slider4.webp";
-import Img5 from "../Images/Slider5.webp";
-import Img6 from "../Images/Slider6.webp";
+import Img1 from "../assets/Images/Slider.webp";
+import Img2 from "../assets/Images/Slider2.webp";
+import Img3 from "../assets/Images/Slider3.webp";
+import Img4 from "../assets/Images/Slider4.webp";
+import Img5 from "../assets/Images/Slider5.webp";
+import Img6 from "../assets/Images/Slider6.webp";
 
-import Sueli from "../Images/Sueli.webp";
+import Sueli from "../assets/Images/Sueli.webp";
 const images = [Img1, Img2, Img3, Img4, Img5, Img6];
 
 function Herosection() {
@@ -18,19 +18,19 @@ function Herosection() {
 
   // Troca automática dos slides
   useEffect(() => {
-    const timer = setTimeout(nextSlide, 8000); // 8 segundos
+    const timer = setTimeout(nextSlide, 7000); // 8 segundos
     return () => clearTimeout(timer);
   }, [index]);
 
   return (
-    <div className="relative w-full h-[636px] flex items-center justify-center overflow-hidden px-8 py-8">
+    <div className="relative w-full flex items-center justify-center overflow-hidden px-8 py-8">
       <AnimatePresence mode="wait">
         <motion.div
           key={index}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 1.8 }}
+          transition={{ duration: 1 }}
           className="absolute inset-0 w-full h-full"
           style={{
             backgroundImage: `url(${images[index]})`,
@@ -40,25 +40,25 @@ function Herosection() {
         />
       </AnimatePresence>
       <motion.div
-        className="flex flex-row items-center w-auto h-auto z-10 gap-40"
-        initial={{ opacity: 0, y: 40 }}
+        className="md:flex flex-row items-center w-auto h-auto z-10 gap-40 "
+        initial={{ opacity: 0, y: 60 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
-        <h1 className="md:text-2xl lg:text-4xl font-extrabold text-white text-left max-w-auto p-6 rounded-xl bg-black bg-opacity-40 shadow-xl backdrop-blur-sm border-l-4 border-pink-400">
-          <span className="block mb-2">
+        <h1 className=" md:text-2xl lg:text-4xl font-extrabold text-white text-left max-w-auto p-6 rounded-xl bg-black bg-opacity-40 shadow-xl backdrop-blur-sm border-l-4 border-pink-400">
+          <span className="block text-gray-200 mb-2">
             Bem-vindo à Sueli Saraiva Nail Designer!
           </span>
           <span className="font-semibold text-pink-400 block mb-5">
             Cuidamos da beleza e saúde das suas unhas com carinho e
             profissionalismo.
           </span>
-          <span className="text-base font-light text-white block">
+          <span className="text-base font-light text-white block text-gray-200">
             Aqui, cada detalhe importa para que você tenha momentos de
             autocuidado, confiança e estilo.
           </span>
         </h1>
-        <div className="w-100 h-100 flex items-center justify-center">
+        <div className=" flex items-center justify-center md:w-100 h-100 flex items-center justify-center">
           <img
             src={Sueli}
             alt="Sueli"
@@ -70,7 +70,7 @@ function Herosection() {
         </div>
       </motion.div>
       {/* Seletor de slides */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex gap-4 z-20">
+      <div className="hidden md:flex absolute bottom-8 left-1/2 -translate-x-1/2 gap-4 z-20">
         {images.map((_, i) => (
           <button
             key={i}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      /* Configuração por formato */
+      png: {
+        quality: 80, // compressão
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80, // conversão para webp
+      },
+      avif: {
+        quality: 70,
+      },
+      svg: {
+        multipass: true,
+      },
+    }),
+  ],
+});
